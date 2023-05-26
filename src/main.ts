b46d798f378e7d286bf37dca799fc8580e553d4a
Dariusz Syncerek <dsyncerek@outlook.com>
2023-05-26 15:22:49 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import axios from 'axios';
+
+const WEBHOOK_URL =
+  'https://discord.com/api/webhooks/1020292115101667368/lS7DoJGWo1AmWQcSWH7j9b1EbizVYG95Sfm6zexaoGv1P4SiDTN0ODBYqMhKvp5xQMpr';
+
+async function run() {
+  const { payload, runId, ref } = github.context;
+  const branch = ref.split('/').at(-1);
+
+  const embed = {
+    timestamp: new Date().toISOString(),
+    description: '👍',
+    color: 0x7fff00,
+    title: `${payload.repository?.full_name}@${branch}`,
+    fields: [
+      {
+        name: 'Commit',
+        value: payload.head_commit ? `[${payload.head_commit.message}](${payload.head_commit.url})` : 'manual trigger',
+      },
+      {
+        name: 'Action',
+        value: `[${runId}](${payload.repository?.html_url}/actions/runs/${runId})`,
+      },
+    ],
+    author: {
+      name: payload.sender?.login,
+      icon_url: payload.sender?.avatar_url,
+      url: payload.sender?.url,
+    },
+    footer: {
+      text: 'cloudticon',
+    },
+    provider: {
+      name: 'github',
+      url: 'https://github.com/',
+    },
+  };
+
+  try {
+    await axios.post(WEBHOOK_URL, { content: '@everyone', embeds: [embed] });
+  } catch (error) {
+    core.setFailed(JSON.stringify(error));
+  }
+}
+
+run();
